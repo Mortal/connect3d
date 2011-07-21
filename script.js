201e68be458fb2d0b922ee09c28ff2d9ba44e4d3
Mathias Rav <rav@cs.au.dk>
2011-07-21 23:36:36 +0200

    Mouse controllable! Oh man

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 "use strict";
 
-var camera, scene, geometry, material, renderer, board;
+var scenewidth, sceneheight, camera, scene, geometry, material, renderer, board, mouse = {down: false}, camerapitch, camerayaw, projector;
 
 window.addEventListener('load', init);
+
+/**
+ * Provides requestAnimationFrame in a cross browser way.
+ * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+ */
+
+if ( !window.requestAnimationFrame ) {
+
+  window.requestAnimationFrame = ( function() {
+
+    return window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+
+      window.setTimeout( callback, 1000 / 60 );
+
+    };
+
+  } )();
+
+}
+
 
 var Player = {RED: 1, BLUE: 2};
 
@@ -27,6 +51,8 @@ function Board() {
 
   this.clearPills();
 
+  this.hoverPin = null;
+
   //this.placePill(0, 0, Player.BLUE);
   //this.placePill(3, 3, Player.RED);
   //this.placePill(3, 0, Player.BLUE);
@@ -50,22 +76,109 @@ Board.prototype.placePill = function (x, y, ply, noWinDetect) {
   return z;
 };
 
+var camdist = 1600, camfov = 45;
+
 function init() {
-  var width = window.innerWidth-16, height = window.innerHeight-16;
+  var width = scenewidth = window.innerWidth-16, height = sceneheight = window.innerHeight-16;
 
   board = new Board();
 
-  camera = new THREE.Camera(75, width / height, 1, 10000);
-  camera.position.z = 1000;
-  camera.position.x = 100;
-  camera.position.y = 500;
+  camera = new THREE.Camera(camfov, width / height, 1, 10000);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(width, height);
 
+  projector = new THREE.Projector();
+
   document.body.appendChild(renderer.domElement);
 
+  setCameraPitchYaw(Math.PI/6, Math.PI/8);
+
+  window.addEventListener('mousedown', mousedown);
+  window.addEventListener('mousemove', mousemove);
+  window.addEventListener('mouseup', mouseup);
+
+  loop();
+}
+
+function loop() {
+  window.requestAnimationFrame(loop); // call me again sometime!
+
+  var hover = null;
+
+  if ('undefined' != typeof mouse.x && !mouse.down) {
+    var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+    projector.unprojectVector( vector, camera );
+
+    var ray = new THREE.Ray( camera.position, vector.subSelf( camera.position ).normalize() );
+
+    hover = THREE.Collisions.rayCastNearest( ray );
+
+    if (board.hoverPin != hover) {
+      if (board.hoverPin) {
+	board.hoverPin.mesh.materials[0].color.setHex(pincolor);
+      }
+      if (hover) {
+	hover.mesh.materials[0].color.setHex(pinhovercolor);
+      }
+      board.hoverPin = hover;
+    }
+  }
+
   render();
+}
+
+function mousedown(event) {
+  mouse.px = event.clientX;
+  mouse.py = event.clientY;
+  mouse.x = -1 + event.clientX / scenewidth * 2;
+  mouse.y = 1 - event.clientY / sceneheight * 2;
+  mouse.moved = false;
+  mouse.down = true;
+}
+
+function mousemove(event) {
+  if (mouse.down) {
+    mouse.moved = true;
+    var dx = mouse.px - event.clientX;
+    var dy = mouse.py - event.clientY;
+    camerapitch += Math.PI*dx/200;
+    while (camerapitch < 0)
+      camerapitch += 2*Math.PI;
+    camerapitch %= 2*Math.PI;
+    camerayaw += -Math.PI*dy/200;
+    camerayaw = Math.min(Math.max(camerayaw, Math.PI/180), 89*Math.PI/180);
+    setCameraPitchYaw(camerapitch, camerayaw);
+  }
+  mouse.px = event.clientX;
+  mouse.py = event.clientY;
+  mouse.x = -1 + event.clientX / scenewidth * 2;
+  mouse.y = 1 - event.clientY / sceneheight * 2;
+}
+
+function mouseup(event) {
+  if (mouse.down && !mouse.moved) {
+    mouseclick(event);
+  }
+  mouse.down = false;
+  mouse.px = event.clientX;
+  mouse.py = event.clientY;
+  mouse.x = -1 + event.clientX / scenewidth * 2;
+  mouse.y = 1 - event.clientY / sceneheight * 2;
+}
+
+function mouseclick(event) {
+  if (board.hoverPin) {
+    place(board.hoverPin.x, board.hoverPin.y);
+  }
+}
+
+function setCameraPitchYaw(pitch, yaw) {
+  camerapitch = pitch;
+  camerayaw = yaw;
+  camera.position.x = camdist*Math.cos(yaw)*Math.sin(pitch);
+  camera.position.z = camdist*Math.cos(yaw)*Math.cos(pitch);
+  camera.position.y = camdist*Math.sin(yaw);
 }
 
 function render() {
@@ -77,7 +190,6 @@ var turn = Player.BLUE;
 function place(x, y) {
   board.placePill(x, y, turn);
   turn = (turn == Player.BLUE) ? Player.RED : Player.BLUE;
-  render();
 }
 
 Board.prototype.clearPills = function () {
@@ -95,15 +207,23 @@ Board.makeBoardMesh = function () {
   return mesh;
 };
 
+var pincolor = 0xCCCC99;
+var pinhovercolor = 0xFFFF99;
+
 Board.makePin = function (x, y) {
+  var r = 20; // radius
   var height = 400;
-  var material = new THREE.MeshBasicMaterial({ color: 0xCCCC99 });
-  var geometry = new THREE.CylinderGeometry(6, 20, 20, height);
-  var mesh = new THREE.Mesh(geometry, material);
+  var geometry = new THREE.CylinderGeometry(6, r, r, height);
+  var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xCCCC99 }));
   mesh.rotation.x = -90 * Math.PI / 180;
-  mesh.position.x = x*300 - 450;
-  mesh.position.z = y*300 - 450;
-  mesh.position.y = height/2;
+  var px = mesh.position.x = x*300 - 450;
+  var pz = mesh.position.z = y*300 - 450;
+  var py = mesh.position.y = height/2;
+  var coll = new THREE.CollisionUtils.MeshOBB(mesh);
+  coll.x = x;
+  coll.y = y;
+  coll.mesh = mesh;
+  THREE.Collisions.colliders.push(coll);
   return mesh;
 };
 
@@ -181,7 +301,6 @@ Board.prototype.winTest = function () {
     var test = testcases_win[i];
     for (var j = 0; j < test.length; j += 3) {
       var z = this.placePill(test[j], test[j+1], test[j+2], 1);
-      render();
       var win = this.winDetect(test[j], test[j+1], z);
       if (!win == (j == test.length-3)) {
 	if (win) {
